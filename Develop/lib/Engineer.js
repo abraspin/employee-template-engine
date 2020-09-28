60e76ff4df36229709a57177c630dd332041077d
abraspin <abrahamspindel@gmail.com>
2020-09-28 12:38:35 -0500

    engineer class complete, engineer.test corrected, passes all tests

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,19 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+// TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+
+const Employee = require("./Employee");
+
+class Engineer extends Employee {
+  constructor(name, id, email, gitHubUserName) {
+    super(name, id, email);
+    this.gitHubUserName = gitHubUserName;
+  }
+  getGithub() {
+    return this.gitHubUserName;
+  }
+  getRole() {
+    return "Engineer";
+  }
+}
+
+module.exports = Engineer;
